9089269bf7e7db2c7925caddfa5130323b2bc2f1
jhongomes <jhonatangomes859@gmail.com>
2021-02-05 16:43:20 -0400

    Update places

--- src/controllers/PlacesControllers.ts
+++ src/controllers/PlacesControllers.ts
@@ -3,7 +3,7 @@ import Places from "../models/Places";
 
 import PlacesRepository from "../repositories/PlacesRepository";
 import CreatePlacesService from "../services/CreatePlacesService";
-
+import UpdatePlacesService from "../services/UpdatePlacesService";
 
 
 class PlacesController{
@@ -34,6 +34,22 @@ class PlacesController{
         return response.status(201).json(places);
 
     }// fim create
+
+
+    public async Update( request: Request, response: Response): Promise<Response>{
+        const { id } = request.params;
+        const { local, meta } = request.body;
+
+        const placesRepository = new PlacesRepository();
+        const updatePlace = new UpdatePlacesService(placesRepository);
+
+        const place = await updatePlace.execute({
+            id,
+            local,
+            meta,
+        });
+        return response.status(201).json(place);
+    }
 
 }
 export default PlacesController;
--- src/routes/places.ts
+++ src/routes/places.ts
@@ -6,8 +6,8 @@ import PlacesController from '../controllers/PlacesControllers'
 const placesRouter = Router();
 const placesController = new PlacesController();
 
-
-placesRouter.post('/', placesController.create);
 placesRouter.get('/', placesController.listar);
+placesRouter.post('/', placesController.create);
+placesRouter.put('/:id', placesController.Update);
 
 export default placesRouter;
